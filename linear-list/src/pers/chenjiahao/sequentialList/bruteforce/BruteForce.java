7bafae23b3687234cb75ec8e5ebd01d0181fd177
chenjiahao <834713285@qq.com>
2024-09-01 13:42:54 +0800

    init

---
+++ linear-list/src/pers/chenjiahao/sequentialList/bruteforce/BruteForce.java
@@ -0,0 +1,10 @@
+package pers.chenjiahao.sequentialList.bruteforce;
+
+/**
+ * 线性枚举(穷举法、暴力法)
+ * [1. 两数之和](https://leetcode.cn/problems/two-sum/)
+ * @author ChenJiahao(五条)
+ * @date 2024/09/01 13:42:19
+ */
+public class BruteForce {
+}
